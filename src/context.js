742f3762974556a3ca49e03feeeeaca5c67ab6c7
vonrimS <mr.mytab@gmail.com>
2022-08-21 11:54:17 +0200

    fetching stories

--- src/context.js
+++ src/context.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useReducer } from 'react'
+import React, { useContext, useEffect, useReducer } from 'react';
 
 import {
   SET_LOADING,
@@ -6,21 +6,48 @@ import {
   REMOVE_STORY,
   HANDLE_PAGE,
   HANDLE_SEARCH,
-} from './actions'
-import reducer from './reducer'
+} from './actions';
+import reducer from './reducer';
 
-const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?';
 
-const initialState = {}
+const initialState = {
+  isLoading: true,
+  hits: [],
+  query: 'react',
+  page: 0,
+  nbPages: 0
+};
 
-const AppContext = React.createContext()
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const fetchStories = async (url) => {
+    dispatch({ type: SET_LOADING });
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      // console.log(data);
+      dispatch({
+        type: SET_STORIES,
+        payload: { hits: data.hits, nbPages: data.nbPages }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchStories(`${API_ENDPOINT}query=${state.query}`);
+  }, []);
+
+  return <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>;
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
